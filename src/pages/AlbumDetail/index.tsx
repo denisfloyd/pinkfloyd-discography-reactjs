ae48c23b547eeafcf5a8d2b7c6e0c9cfd2b40313
denis.ladeira <denisladeira1@gmail.com>
2020-10-11 11:46:31 -0300

    Make some tests in react-player

--- src/pages/AlbumDetail/index.tsx
+++ src/pages/AlbumDetail/index.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactPlayer from 'react-player/lazy';
+import {} from 'react-player';
+
+// interface YouTubePlayerProps {}
+
+interface PlayerProps extends ReactPlayer {
+  getInternalPlayer(): any;
+}
 
 const AlbumDetail: React.FC = () => {
-  return <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />;
+  const playerRef = useRef<PlayerProps>(null);
+
+  const showPlaylist = (): any => {
+    console.log(playerRef?.current?.getInternalPlayer().getVideoData());
+    return playerRef.current?.getInternalPlayer().nextVideo();
+  };
+
+  function tempoDecorrido(v: number): void {
+    console.log(v);
+  }
+
+  return (
+    <>
+      <ReactPlayer
+        ref={playerRef}
+        url="https://www.youtube.com/watch?v=nHER1RmSqA8&list=PL6ogdCG3tAWjWskN7Av6hzlVa9GE15ofK"
+        controls
+        onDuration={tempoDecorrido}
+        // config={{
+        //   youtube: {
+        //     embedOptions: {
+
+        //     },
+        //   },
+        // }}
+      />
+
+      <button type="button" onClick={showPlaylist}>
+        next
+      </button>
+    </>
+  );
 };
 
 export default AlbumDetail;
