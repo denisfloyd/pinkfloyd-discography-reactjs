64a13fa8b46760ec94a44843a81b03eb4994dc3b
denis.ladeira <denisladeira1@gmail.com>
2020-10-11 14:14:59 -0300

    pinkfloyd data array

--- src/data/info.ts
+++ src/data/info.ts
@@ -103,6 +103,13 @@ export const pinkFloydAlbunsArray: Array<Album> = [
     image:
       'https://upload.wikimedia.org/wikipedia/pt/0/0e/Pink_Floyd_-_Meddle.jpg',
     year: 1971,
+    playlist: [
+      'One of These Days',
+      'A Pillow of Winds',
+      'Fearless',
+      'San Tropez',
+      'Seamus',
+    ],
   },
   {
     id: 7,
@@ -110,6 +117,18 @@ export const pinkFloydAlbunsArray: Array<Album> = [
     image:
       'https://upload.wikimedia.org/wikipedia/pt/thumb/3/37/Pink_Floyd_-_1972_-_Obscured_by_Clouds.jpg/220px-Pink_Floyd_-_1972_-_Obscured_by_Clouds.jpg',
     year: 1972,
+    playlist: [
+      'Obscured by Clouds',
+      "When You're In",
+      'Burning Bridges',
+      "The Gold It's in the...",
+      "Wot's... Uh The Deal",
+      'Mudmen',
+      "Childhood's End",
+      'Free Four',
+      'Stay',
+      'Absolutely Curtains',
+    ],
   },
   {
     id: 8,
@@ -117,6 +136,18 @@ export const pinkFloydAlbunsArray: Array<Album> = [
     image:
       'https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png',
     year: 1973,
+    playlist: [
+      'Speak to Me',
+      'Breathe',
+      'On the Run',
+      'Time',
+      'The Great Gig in the Sky',
+      'Money',
+      'Us and Them',
+      'Any Colour You Like',
+      'Brain Damage',
+      'Eclipse',
+    ],
   },
   {
     id: 9,
@@ -124,6 +155,13 @@ export const pinkFloydAlbunsArray: Array<Album> = [
     image:
       'https://upload.wikimedia.org/wikipedia/pt/b/b7/Wish_You_Were_Here.jpg',
     year: 1975,
+    playlist: [
+      'Shine On You Crazy Diamond',
+      'Welcome to The Machine',
+      'Have a Cigar',
+      'Wish You Were Here',
+      'Shine On You Crazy Diamond',
+    ],
   },
   {
     id: 10,
@@ -131,6 +169,13 @@ export const pinkFloydAlbunsArray: Array<Album> = [
     image:
       'https://upload.wikimedia.org/wikipedia/pt/thumb/4/4a/Pink_Floyd_-_Animals.jpg/220px-Pink_Floyd_-_Animals.jpg',
     year: 1977,
+    playlist: [
+      'Pigs on the Wing (Part I)',
+      'Dogs',
+      'Pigs (Three Different Ones)',
+      'Sheep',
+      'Pigs on the Wing (Part II)',
+    ],
   },
   {
     id: 11,
@@ -138,6 +183,34 @@ export const pinkFloydAlbunsArray: Array<Album> = [
     image:
       'https://images-na.ssl-images-amazon.com/images/I/81QzRZ4UoNL._AC_SX466_.jpg',
     year: 1979,
+    playlist: [
+      'In the Flesh?',
+      'The Thin Ice',
+      'Another Brick in the Wall (Part I)',
+      'The Happiest Days of Our Lives',
+      'Another Brick in the Wall (Part II)',
+      'Mother',
+      'Goodbye Blue Sky',
+      'Empty Spaces',
+      'Young Lust',
+      'One of My Turns',
+      "Don't Leave Me Now",
+      'Another Brick in the Wall (Part III)',
+      'Goodbye Cruel World',
+      'Hey You',
+      'Is There Anybody Out There?',
+      'Nobody Home',
+      'Vera',
+      'Bring the Boys Back Home',
+      'Comfortably Numb',
+      'The Show Must Go On',
+      'In the Flesh',
+      'Run Like Hell',
+      'Waiting for the Worms',
+      'Stop',
+      'The Trial',
+      'Outside the Wall',
+    ],
   },
   {
     id: 12,
@@ -145,6 +218,20 @@ export const pinkFloydAlbunsArray: Array<Album> = [
     image:
       'https://upload.wikimedia.org/wikipedia/pt/f/f7/Pink_Floyd_-_The_Final_Cut.jpg',
     year: 1983,
+    playlist: [
+      'The Post War Dream',
+      'Your Possible Pasts',
+      'One of the Few',
+      "The Hero's Return",
+      "The Gunner's Dream",
+      'Paranoid Eyes',
+      'Get Your Filthy Hands Off My Desert',
+      'The Fletcher Memorial Home',
+      'Southampton Dock',
+      'The Final Cut',
+      'Not Now John',
+      'Two Suns in the Sunset',
+    ],
   },
   {
     id: 13,
@@ -152,6 +239,18 @@ export const pinkFloydAlbunsArray: Array<Album> = [
     image:
       'https://upload.wikimedia.org/wikipedia/pt/3/3c/Pink_Floyd_-_A_Momentary_Lapse_of_Reason.jpg',
     year: 1987,
+    playlist: [
+      'Signs of Life',
+      'Learning to Fly',
+      'The Dogs of War',
+      'One Slip',
+      'On the Turning Away',
+      '"Yet Another Movie" / "Round and Around"',
+      'A New Machine (Part 1)',
+      'Terminal Frost',
+      'A New Machine (Part 2)',
+      'Sorrow',
+    ],
   },
   {
     id: 14,
@@ -159,6 +258,19 @@ export const pinkFloydAlbunsArray: Array<Album> = [
     image:
       'https://upload.wikimedia.org/wikipedia/en/6/6e/Pink_Floyd_-_Division_Bell.jpg',
     year: 1994,
+    playlist: [
+      'Cluster One',
+      'What Do You Want from Me',
+      'Poles Apart',
+      'Marooned',
+      'A Great Day for Freedom',
+      'Wearing the Inside Out',
+      'Take It Back',
+      'Coming Back to Life',
+      'Keep Talking',
+      'Lost for Words',
+      'High Hopes',
+    ],
   },
   {
     id: 15,
@@ -166,6 +278,26 @@ export const pinkFloydAlbunsArray: Array<Album> = [
     image:
       'https://upload.wikimedia.org/wikipedia/pt/0/04/The_Endless_River_-_Pink_Floyd_-_2014.jpg',
     year: 2014,
+    playlist: [
+      'Things Left Unsaid',
+      "It's What We Do",
+      'Ebb and Flow',
+      'Sum',
+      'Skins',
+      'Unsung',
+      'Anisina',
+      'The Lost Art of Conversation',
+      'On Noodle Street',
+      'Night Light',
+      'Allons-y (1)',
+      "Autumn '68",
+      'Allons-y (2)',
+      "Talkin' Hawkin",
+      'Calling',
+      'Eyes to Pearls',
+      'Surfacing',
+      'Louder than Words',
+    ],
   },
   {
     id: 16,
