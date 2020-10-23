ab3b596d11901f81a7b69aea3950803256ff1fb8
denis.ladeira <denisladeira1@gmail.com>
2020-10-23 16:04:00 -0300

    get route params

--- src/pages/AlbumDetail/index.tsx
+++ src/pages/AlbumDetail/index.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
 import ReactPlayer from 'react-player/lazy';
 import {} from 'react-player';
 
 import { Album, pinkFloydAlbunsArray as AlbumArray } from '../../data/info';
+
+import { Container } from './styles';
 
 // interface YouTubePlayerProps {}
 
@@ -11,17 +15,21 @@ interface PlayerProps extends ReactPlayer {
 }
 
 interface AlbumDetailProps {
-  albumId: number;
+  albumId: string;
 }
 
-const AlbumDetail: React.FC<AlbumDetailProps> = ({ albumId }) => {
+const AlbumDetail: React.FC = () => {
+  const {
+    params: { albumId },
+  } = useRouteMatch<AlbumDetailProps>();
+
   const [album, setAlbum] = useState({} as Album);
 
   const playerRef = useRef<PlayerProps>(null);
 
   useEffect(() => {
     const albumFromQueryParams = AlbumArray.filter(albumObject => {
-      return albumObject.id === albumId;
+      return albumObject.id === parseInt(albumId, 2);
     });
 
     if (albumFromQueryParams) {
@@ -30,21 +38,24 @@ const AlbumDetail: React.FC<AlbumDetailProps> = ({ albumId }) => {
   }, [albumId]);
 
   const showPlaylist = (): any => {
-    console.log(playerRef?.current?.getInternalPlayer().getVideoData());
+    // console.log(playerRef?.current?.getInternalPlayer().getVideoData());
     return playerRef.current?.getInternalPlayer().nextVideo();
   };
 
-  function tempoDecorrido(v: number): void {
-    console.log(v);
-  }
-
   return (
-    <>
+    <Container>
+      {album.playlist && (
+        <ul>
+          {album.playlist.map(music => (
+            <li>{music}</li>
+          ))}
+        </ul>
+      )}
+
       <ReactPlayer
         ref={playerRef}
         url="https://www.youtube.com/watch?v=nHER1RmSqA8&list=PL6ogdCG3tAWjWskN7Av6hzlVa9GE15ofK"
         controls
-        onDuration={tempoDecorrido}
         // config={{
         //   youtube: {
         //     embedOptions: {
@@ -57,7 +68,8 @@ const AlbumDetail: React.FC<AlbumDetailProps> = ({ albumId }) => {
       <button type="button" onClick={showPlaylist}>
         next
       </button>
-    </>
+    </Container>
+    // <AlbumCover />
   );
 };
 
