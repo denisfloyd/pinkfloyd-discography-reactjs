86515e82fdb7839500985ea93458ae406a339ff0
denismend <denisladeira1@gmail.com>
2020-11-05 09:34:06 -0300

    svg playing style to global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -22,4 +22,40 @@ export default createGlobalStyle`
   button {
     cursor: pointer;
   }
+
+  @keyframes loading-bar-morph {
+    0% {
+      transform: scaleY(1);
+    }
+    25% {
+      transform: scaleY(0.3);
+    }
+    50% {
+      transform: scaleY(0.7);
+    }
+    75% {
+      transform: scaleY(0.15);
+    }
+    9% {
+      transform: scaleY(1)
+    }
+  }
+
+  svg.music-playing-svg {
+    fill: ${props => props.theme.colors.text};
+    opacity: 1;
+
+    rect#loading-bar-left {
+      animation: loading-bar-morph 1s linear 0.1s infinite;
+      transform-origin: center;
+    }
+    rect#loading-bar-middle {
+      animation: loading-bar-morph 1s linear 0.2s infinite;
+      transform-origin: center;
+    }
+    rect#loading-bar-right {
+      animation: loading-bar-morph 1s linear 0.4s infinite;
+      transform-origin: center;
+    }
+  }
 `;
