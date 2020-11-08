40e19b43063195726630aca38aacd4a03f1bccb8
denismend <denisladeira1@gmail.com>
2020-11-08 14:54:28 -0300

    create theme hook

---
+++ src/hooks/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ThemeProvider } from './theme';
+
+const AppProvider: React.FC = ({ children }) => (
+  <ThemeProvider>{children}</ThemeProvider>
+);
+
+export default AppProvider;
---
+++ src/hooks/theme.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useState, useContext } from 'react';
+
+interface ThemeContextData {
+  isDarkTheme: boolean;
+  setIsDarkTheme(newValue: boolean): void;
+}
+
+const ThemeContext = createContext<ThemeContextData>({} as ThemeContextData);
+
+const ThemeProvider: React.FC = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(true);
+
+  return (
+    <ThemeContext.Provider value={{ isDarkTheme, setIsDarkTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+function useTheme(): ThemeContextData {
+  const context = useContext(ThemeContext);
+
+  // if (!context) {
+  //   throw new Error('useTheme must be used within an ThemeProvider');
+  // }
+
+  return context;
+}
+
+export { ThemeProvider, useTheme };
